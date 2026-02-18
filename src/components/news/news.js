6b98df69e1e236bad05212db2b2b6114f721e950
Paweł Wrona <pawelwrona2009@gmail.com>
2026-02-18 15:20:45 +0100

    likes and dislikes

--- src/components/news/news.js
+++ src/components/news/news.js
@@ -1,19 +1,29 @@
+import { useState } from "react";
 import "./news.css";
 
 export function News({ title, description }) {
-  const handleClick = (name) => {
-    alert(`${name}, Kliknieto przyciisk`);
+  const [likes, setLikes] = useState(0);
+
+  const increaseLikes = (value) => {
+    if (likes < 1 && value < 0) {
+      return;
+    }
+
+    setLikes(likes + value);
   };
+
   return (
     <div className="glass-card">
       <h3>{title}</h3>
       <p>{description}</p>
-
+      <span>{likes}</span>
       <div className="buttons">
-        <button onClick={() => handleClick("Kacper")} className="like">
+        <button onClick={() => increaseLikes(1)} className="like">
           LIKE
         </button>
-        <button className="dislike">DISLIKE</button>
+        <button onClick={() => increaseLikes(-1)} className="dislike">
+          DISLIKE
+        </button>
       </div>
     </div>
   );
