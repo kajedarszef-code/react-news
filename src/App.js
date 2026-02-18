5dbeea7fea222928d6efeeb353b196e1d091944b
Paweł Wrona <pawelwrona2009@gmail.com>
2026-02-18 13:25:11 +0100

    dodanie newsow

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import {News} from "./components/news";
+function App() {
+
+  const name = "Kacper";
+
+  const newsList = [
+    {
+      title: "Spotkanie w Monachium",
+      description:
+        "Odbyło się spotkanie światowych liderów w Monachium dotyczące bezpieczeństwa i współpracy międzynarodowej.",
+    },
+    {
+      title: "Nowa misja kosmiczna",
+      description:
+        "Europejska Agencja Kosmiczna ogłosiła start nowej misji badawczej na Marsa.",
+    },
+    {
+      title: "Rekord na giełdzie",
+      description:
+        "Warszawska giełda odnotowała najwyższy wzrost indeksu WIG w tym kwartale.",
+    },
+    {
+      title: "Przełom w medycynie",
+      description:
+        "Naukowcy opracowali nową metodę wczesnego wykrywania chorób serca.",
+    },
+    {
+      title: "Nowe technologie AI",
+      description:
+        "Powstał innowacyjny model sztucznej inteligencji wspierający edukację i analizę danych.",
+    },
+    {
+      title: "Zmiany klimatyczne",
+      description:
+        "Opublikowano raport wskazujący na przyspieszone tempo topnienia lodowców.",
+    },
+  ];
+
+  return (
+    <>
+        <h1 className='crazy-title'>Hello {name}</h1>
+
+      <main>
+        {newsList.map(news => <News></News>)}
+        </main>
+
+      </>
+  );
+}
+
+export default App;
---
+++ src/components/news.js
@@ -0,0 +1,15 @@
+import "./news.css";
+
+export function News() {
+  return (
+    <div class="glass-card">
+      <h3>TITLE</h3>
+      <p>DESCRIPTION</p>
+
+      <div class="buttons">
+        <button class="like">LIKE</button>
+        <button class="dislike">DISLIKE</button>
+      </div>
+    </div>
+  );
+}
